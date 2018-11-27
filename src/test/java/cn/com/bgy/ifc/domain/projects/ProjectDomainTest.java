47c3a7cc405088698cdfc22e7f973ab7e90f05fd
duhj <dhjdxl19921104>
2018-11-27 23:08:14 +0800

    初始化

---
+++ src/test/java/cn/com/bgy/ifc/domain/projects/ProjectDomainTest.java
@@ -0,0 +1,10 @@
+package cn.com.bgy.ifc.domain.projects;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ProjectDomainTest {
+}
