73e78a883ebaecece5c0efbc6d9279c2df1adc47
gulezhidian <842693323@qq.com>
2018-12-04 13:37:12 +0800

    基础代码层修改

--- src/main/java/cn/com/bgy/ifc/entity/vo/basic/UserVo.java
+++ src/main/java/cn/com/bgy/ifc/entity/vo/basic/UserVo.java
@@ -1,15 +1,22 @@
 package cn.com.bgy.ifc.entity.vo.basic;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class UserVo {
     private Long id;
 
+    @NotBlank(message="用户名不能为空")
     private String userName;
-
+    @NotBlank(message="手机号码不能为空")
+    @Size(min=11,max=11,message="手机号码长度不正确")
     private String phone;
-
+    @NotBlank(message="邮箱不能为空")
     private String email;
-
+    @NotBlank(message="密码不能为空")
     private String password;
+    @NotNull(message="部门ID不能为空")
 
     private Long deptId;
 
