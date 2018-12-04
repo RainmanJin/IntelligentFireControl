8b33d1161019860716b3fc0effc3cf7126dcd7e8
Administrator <15882380729@163.com>
2018-12-04 17:49:01 +0800

    log4g测试

--- src/main/java/cn/com/bgy/ifc/controller/inner/IndexController.java
+++ src/main/java/cn/com/bgy/ifc/controller/inner/IndexController.java
@@ -1,11 +1,14 @@
 package cn.com.bgy.ifc.controller.inner;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
-
+	protected final Logger logger = LoggerFactory.getLogger(this.getClass());
 	@GetMapping("/index")
 	public String index() {
 		return "index";
@@ -29,4 +32,12 @@ public class IndexController {
 	public String mainContent() {
 		return "mainContent";
 	}
+    @RequestMapping("/test")
+    public String xslsheet(){
+		logger.trace("I am trace log.");
+		logger.debug("I am debug log.");
+		logger.warn("I am warn log.");
+		logger.error("I am error log.");
+		return "top";
+    }
 }
