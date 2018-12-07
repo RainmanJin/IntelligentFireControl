b2dbbbeef19d2b151c6c9d89d5df4bbe87c259f0
huxin <15882380729@163.com>
2018-12-07 10:41:58 +0800

    日志controller

---
+++ src/main/java/cn/com/bgy/ifc/controller/inner/basic/SystemLogController.java
@@ -0,0 +1,44 @@
+package cn.com.bgy.ifc.controller.inner.basic;
+
+import cn.com.bgy.ifc.domain.interfaces.basic.SystemLogDomain;
+import cn.com.bgy.ifc.entity.po.basic.SystemOperationLog;
+import cn.com.bgy.ifc.entity.vo.ResponseVO;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Map;
+
+/**
+ * @Author huxin
+ * @Date 2018/12/7 10:13
+ * @Description  系统日志查询
+ **/
+@Controller
+public class SystemLogController {
+
+    @Autowired
+    SystemLogDomain systemLogDomain;
+    /**
+     * @Author huxin
+     * @Description 系统日志查询及分页
+     * @Date 2018/12/7 10:14
+     */
+    @GetMapping("query")
+    public ResponseVO<PageInfo<SystemOperationLog>> querySystemLogInfo( Page<SystemOperationLog> page){
+        PageInfo<SystemOperationLog> pageInfo = systemLogDomain.querySystemLogInfo(page);
+        return ResponseVO.<PageInfo<SystemOperationLog>>success().setData(pageInfo);
+    }
+    /**
+     * @Author huxin
+     * @Description 多条件查询日志及分页
+     * @Date 2018/12/7 10:16
+     */
+    @GetMapping("queryRequirement")
+    public ResponseVO<PageInfo<SystemOperationLog>> queryRequirementSytemLogInfo( Page<SystemOperationLog> page, Map<String,String> map){
+        PageInfo<SystemOperationLog> pageInfo = systemLogDomain.queryRequirementSytemLogInfo(page,map);
+        return ResponseVO.<PageInfo<SystemOperationLog>>success().setData(pageInfo);
+    }
+}
