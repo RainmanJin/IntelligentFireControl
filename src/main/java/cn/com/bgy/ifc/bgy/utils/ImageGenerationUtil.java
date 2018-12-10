754a98d35ee738ab050473c48ac5d3e3ce5eea97
huxin <15882380729@163.com>
2018-12-10 15:15:17 +0800

    验证码生成工具

--- src/main/java/cn/com/bgy/ifc/bgy/utils/ImageGenerationUtil.java
+++ src/main/java/cn/com/bgy/ifc/bgy/utils/ImageGenerationUtil.java
@@ -17,12 +17,12 @@ import java.util.Random;
  **/
 
 public class ImageGenerationUtil {
-    private static int width = 285;// 定义图片的width
-    private static int height = 54;// 定义图片的height
+    private static int width = 130;// 定义图片的width
+    private static int height = 50;// 定义图片的height
     private static int codeCount = 4;// 定义图片上显示验证码的个数
-    private static int xx = 54;
-    private static int fontHeight = 50; //字体高度
-    private static int codeY = 48;
+    private static int xx = 20; //字体从左的起点
+    private static int fontHeight = 40;
+    private static int codeY = 40;//字体从上到下的终点
     private static char[] codeSequence = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 
     /**
@@ -94,10 +94,12 @@ public class ImageGenerationUtil {
      * @Date 2018/12/10 14:11
      */
     public static void main( String[] args ) throws Exception {
-        //创建文件输出流对象
-        OutputStream out = new FileOutputStream("D://img/" + System.currentTimeMillis() + ".jpg");
-        Map<String, Object> map = ImageGenerationUtil.generateCodeAndPic();
-        ImageIO.write((RenderedImage) map.get("codePic"), "jpeg", out);
-        System.out.println("验证码的值为：" + map.get("code"));
+        for (int i = 0; i <2 ; i++) {
+            //创建文件输出流对象
+            OutputStream out = new FileOutputStream("D://img/" + System.currentTimeMillis() + ".jpg");
+            Map<String, Object> map = ImageGenerationUtil.generateCodeAndPic();
+            ImageIO.write((RenderedImage) map.get("codePic"), "jpeg", out);
+            System.out.println("验证码的值为：" + map.get("code"));
+        }
     }
 }
