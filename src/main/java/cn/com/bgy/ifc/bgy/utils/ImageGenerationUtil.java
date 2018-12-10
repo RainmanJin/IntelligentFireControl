05eeebf2c5614c1b859fa072737b5040ef0ff180
huxin <15882380729@163.com>
2018-12-10 14:36:46 +0800

    验证码生成工具

--- src/main/java/cn/com/bgy/ifc/bgy/utils/ImageGenerationUtil.java
+++ src/main/java/cn/com/bgy/ifc/bgy/utils/ImageGenerationUtil.java
@@ -17,12 +17,12 @@ import java.util.Random;
  **/
 
 public class ImageGenerationUtil {
-    private static int width = 90;// 定义图片的width
-    private static int height = 20;// 定义图片的height
+    private static int width = 285;// 定义图片的width
+    private static int height = 54;// 定义图片的height
     private static int codeCount = 4;// 定义图片上显示验证码的个数
-    private static int xx = 15;
-    private static int fontHeight = 18;
-    private static int codeY = 16;
+    private static int xx = 54;
+    private static int fontHeight = 50; //字体高度
+    private static int codeY = 48;
     private static char[] codeSequence = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 
     /**
