11b429cefb14d3f7e31689ab5e2c873cb0a145c1
zc1428580764 <1428580764@qq.com>
2018-12-21 15:08:18 +0800

    代码跟新02

---
+++ src/main/java/cn/com/bgy/ifc/entity/po/equipment/EquipmentType.java
@@ -0,0 +1,43 @@
+package cn.com.bgy.ifc.entity.po.equipment;
+
+public class EquipmentType {
+    private Long id;
+
+    private String name;
+
+    private Long parentId;
+
+    private Integer status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(Long parentId) {
+        this.parentId = parentId;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+}
