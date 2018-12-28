28a4920b25f628b12cc22fce83c7b69683950dfe
chenlie <842693323@qq.com>
2018-12-28 17:03:13 +0800

    merge

---
+++ src/main/java/cn/com/bgy/ifc/domain/interfaces/system/SystemPowerDomain.java
@@ -0,0 +1,26 @@
+package cn.com.bgy.ifc.domain.interfaces.system;
+
+import cn.com.bgy.ifc.domain.interfaces.base.BaseDomain;
+import cn.com.bgy.ifc.entity.po.system.SystemPower;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageInfo;
+
+import java.util.List;
+
+public interface SystemPowerDomain extends BaseDomain<SystemPower> {
+
+    PageInfo<SystemPower> queryListByPage(Page<SystemPower> page, SystemPower record);
+
+    List<SystemPower> queryListByParam(SystemPower record);
+
+    int insert(SystemPower record);
+
+    SystemPower findById(Long id);
+
+    int update(SystemPower record);
+
+    int deleteById(Long id);
+
+    List<SystemPower> queryListByUserId(Long userId);
+
+}
