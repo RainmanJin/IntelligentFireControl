1b72a67a5ced1ea1386e08b04ea023c85d70097c
huxin <15882380729@163.com>
2019-01-12 11:23:35 +0800

    2019年1月12日11:23:28

--- src/main/java/cn/com/bgy/ifc/entity/po/firepatrol/RecordTable.java
+++ src/main/java/cn/com/bgy/ifc/entity/po/firepatrol/RecordTable.java
@@ -4,19 +4,51 @@ import java.util.Date;
 
 public class RecordTable {
     /**
-     * ID
+     * id
      */
     private Long id;
 
     /**
-     * 记录内容
+     * 设施设备ID
      */
-    private String recordContent;
+    private Long equipmentId;
 
     /**
-     * 设备类别ID
+     * 工单ID
      */
-    private Long equipmentTypeId;
+    private Long orderId;
+
+    /**
+     * 情况  0:正常 1：异常
+     */
+    private Integer situation;
+
+    /**
+     * 描述
+     */
+    private String description;
+
+    /**
+     * 
+ 类别  1：巡检  2：维保  3：检测
+     */
+    private Integer type;
+
+    /**
+     * 填写人
+     */
+    private String recordPerson;
+
+    /**
+     * 审核人
+ 
+     */
+    private String approver;
+
+    /**
+     * 审核结果 
+     */
+    private String approvalResult;
 
     /**
      * 是否逻辑删除  0:存在  1：删除
@@ -29,51 +61,155 @@ public class RecordTable {
     private Date createTime;
 
     /**
-     * ID
-     * @return id ID
+     * id
+     * @return id id
      */
     public Long getId() {
         return id;
     }
 
     /**
-     * ID
-     * @param id ID
+     * id
+     * @param id id
      */
     public void setId(Long id) {
         this.id = id;
     }
 
     /**
-     * 记录内容
-     * @return record_content 记录内容
+     * 设施设备ID
+     * @return equipment_id 设施设备ID
      */
-    public String getRecordContent() {
-        return recordContent;
+    public Long getEquipmentId() {
+        return equipmentId;
     }
 
     /**
-     * 记录内容
-     * @param recordContent 记录内容
+     * 设施设备ID
+     * @param equipmentId 设施设备ID
      */
-    public void setRecordContent(String recordContent) {
-        this.recordContent = recordContent;
+    public void setEquipmentId(Long equipmentId) {
+        this.equipmentId = equipmentId;
     }
 
     /**
-     * 设备类别ID
-     * @return equipment_type_id 设备类别ID
+     * 工单ID
+     * @return order_id 工单ID
      */
-    public Long getEquipmentTypeId() {
-        return equipmentTypeId;
+    public Long getOrderId() {
+        return orderId;
     }
 
     /**
-     * 设备类别ID
-     * @param equipmentTypeId 设备类别ID
+     * 工单ID
+     * @param orderId 工单ID
      */
-    public void setEquipmentTypeId(Long equipmentTypeId) {
-        this.equipmentTypeId = equipmentTypeId;
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    /**
+     * 情况  0:正常 1：异常
+     * @return situation 情况  0:正常 1：异常
+     */
+    public Integer getSituation() {
+        return situation;
+    }
+
+    /**
+     * 情况  0:正常 1：异常
+     * @param situation 情况  0:正常 1：异常
+     */
+    public void setSituation(Integer situation) {
+        this.situation = situation;
+    }
+
+    /**
+     * 描述
+     * @return description 描述
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * 描述
+     * @param description 描述
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * 
+ 类别  1：巡检  2：维保  3：检测
+     * @return type 
+ 类别  1：巡检  2：维保  3：检测
+     */
+    public Integer getType() {
+        return type;
+    }
+
+    /**
+     * 
+ 类别  1：巡检  2：维保  3：检测
+     * @param type 
+ 类别  1：巡检  2：维保  3：检测
+     */
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    /**
+     * 填写人
+     * @return record_person 填写人
+     */
+    public String getRecordPerson() {
+        return recordPerson;
+    }
+
+    /**
+     * 填写人
+     * @param recordPerson 填写人
+     */
+    public void setRecordPerson(String recordPerson) {
+        this.recordPerson = recordPerson;
+    }
+
+    /**
+     * 审核人
+ 
+     * @return approver 审核人
+ 
+     */
+    public String getApprover() {
+        return approver;
+    }
+
+    /**
+     * 审核人
+ 
+     * @param approver 审核人
+ 
+     */
+    public void setApprover(String approver) {
+        this.approver = approver;
+    }
+
+    /**
+     * 审核结果 
+     * @return approval_result 审核结果 
+     */
+    public String getApprovalResult() {
+        return approvalResult;
+    }
+
+    /**
+     * 审核结果 
+     * @param approvalResult 审核结果 
+     */
+    public void setApprovalResult(String approvalResult) {
+        this.approvalResult = approvalResult;
     }
 
     /**
