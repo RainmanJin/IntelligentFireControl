864dfd8090e586341dd373acab1e691b011dd24a
huxin <15882380729@163.com>
2019-01-12 10:47:57 +0800

    2019年1月12日10:47:52

--- src/main/java/cn/com/bgy/ifc/dao/firepatrol/RecordByContentDao.java
+++ src/main/java/cn/com/bgy/ifc/dao/firepatrol/RecordByContentDao.java
@@ -1,17 +1,8 @@
 package cn.com.bgy.ifc.dao.firepatrol;
 
+import cn.com.bgy.ifc.dao.base.BaseDao;
 import cn.com.bgy.ifc.entity.po.firepatrol.RecordByContent;
 
-public interface RecordByContentDao {
-    /**
-     *
-     * @mbg.generated 2019-01-12
-     */
-    int insert(RecordByContent record);
+public interface RecordByContentDao extends BaseDao<RecordByContent> {
 
-    /**
-     *
-     * @mbg.generated 2019-01-12
-     */
-    int insertSelective(RecordByContent record);
 }
