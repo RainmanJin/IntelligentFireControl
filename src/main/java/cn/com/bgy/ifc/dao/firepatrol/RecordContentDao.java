6d9d2e464f6906cbbfba57ddce42c15ff070242e
huxin <15882380729@163.com>
2019-01-12 10:48:26 +0800

    2019年1月12日10:48:22

--- src/main/java/cn/com/bgy/ifc/dao/firepatrol/RecordContentDao.java
+++ src/main/java/cn/com/bgy/ifc/dao/firepatrol/RecordContentDao.java
@@ -1,17 +1,8 @@
 package cn.com.bgy.ifc.dao.firepatrol;
 
+import cn.com.bgy.ifc.dao.base.BaseDao;
 import cn.com.bgy.ifc.entity.po.firepatrol.RecordContent;
 
-public interface RecordContentDao {
-    /**
-     *
-     * @mbg.generated 2019-01-12
-     */
-    int insert(RecordContent record);
+public interface RecordContentDao extends BaseDao<RecordContent> {
 
-    /**
-     *
-     * @mbg.generated 2019-01-12
-     */
-    int insertSelective(RecordContent record);
 }
--- src/main/java/cn/com/bgy/ifc/dao/firepatrol/RecordTableDao.java
+++ src/main/java/cn/com/bgy/ifc/dao/firepatrol/RecordTableDao.java
@@ -1,17 +1,8 @@
 package cn.com.bgy.ifc.dao.firepatrol;
 
+import cn.com.bgy.ifc.dao.base.BaseDao;
 import cn.com.bgy.ifc.entity.po.firepatrol.RecordTable;
 
-public interface RecordTableDao {
-    /**
-     *
-     * @mbg.generated 2019-01-12
-     */
-    int insert(RecordTable record);
+public interface RecordTableDao extends BaseDao<RecordTable> {
 
-    /**
-     *
-     * @mbg.generated 2019-01-12
-     */
-    int insertSelective(RecordTable record);
 }
