5df2aaa0f00f4d282d4f7c089e794461071cd9e9
chenlie <842693323@qq.com>
2019-01-18 10:37:35 +0800

    删除不需要的防火巡查计划表

--- src/main/java/cn/com/bgy/ifc/dao/fireinspection/FirePlanTestDao.java
+++
@@ -1,7 +0,0 @@
-package cn.com.bgy.ifc.dao.fireinspection;
-
-import cn.com.bgy.ifc.dao.base.BaseDao;
-import cn.com.bgy.ifc.entity.po.fireinspection.FirePlanTest;
-
-public interface FirePlanTestDao extends BaseDao<FirePlanTest>{
-}
--- src/main/java/cn/com/bgy/ifc/dao/fireinspection/FirePlanTestDetileDao.java
+++
@@ -1,9 +0,0 @@
-package cn.com.bgy.ifc.dao.fireinspection;
-
-import cn.com.bgy.ifc.entity.po.fireinspection.FirePlanTestDetile;
-
-public interface FirePlanTestDetileDao {
-    int insert(FirePlanTestDetile record);
-
-    int insertSelective(FirePlanTestDetile record);
-}
