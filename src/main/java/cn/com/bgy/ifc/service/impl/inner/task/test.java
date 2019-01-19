f656f5a12d90cfb04736e31cce3261d50a2c9a60
huxin <15882380729@163.com>
2019-01-19 09:41:22 +0800

    2019年1月19日09:41:02

--- src/main/java/cn/com/bgy/ifc/service/impl/inner/task/test.java
+++
@@ -1,9 +0,0 @@
-package cn.com.bgy.ifc.service.impl.inner.task;
-
-public class test {
-
-    public static void main( String[] args ) {
-        Integer i = 0;
-        System.out.println(i);
-    }
-}
